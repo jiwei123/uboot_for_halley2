42a79ca878d1738aacb9aa409ccb2d4313ce4ad7
ykli <ykli@ingenic.cn>
2013-11-15 09:29:25 +0800

    use Android-gcc as default compiler

--- arch/mips/config.mk
+++ arch/mips/config.mk
@@ -21,7 +21,7 @@
 # MA 02111-1307 USA
 #
 
-CROSS_COMPILE ?= mips-linux-gnu-
+CROSS_COMPILE ?= mipsel-linux-android-
 
 # Handle special prefix in ELDK 4.0 toolchain
 ifneq (,$(findstring 4KCle,$(CROSS_COMPILE)))
@@ -62,15 +62,15 @@ PLATFORM_CPPFLAGS += -DCONFIG_MIPS -D__MIPS__
 # to RAM, unless we're building SPL which doesn't relocate. $28 is always
 # used as gp.
 #
-PLATFORM_CPPFLAGS		+= -G 0 -mabicalls $(ENDIANNESS)
+PLATFORM_CPPFLAGS		+= -G 0 $(ENDIANNESS)
 PLATFORM_CPPFLAGS		+= -msoft-float
 PLATFORM_LDFLAGS		+= -G 0 -static -n -nostdlib $(ENDIANNESS)
 PLATFORM_RELFLAGS		+= -ffunction-sections -fdata-sections
 LDFLAGS_FINAL			+= --gc-sections
 OBJCFLAGS			+= --remove-section=.dynsym
 ifdef CONFIG_SPL_BUILD
-PLATFORM_CPPFLAGS		+= -fno-pic
+PLATFORM_CPPFLAGS		+= -fno-pic -mno-abicalls
 else
-PLATFORM_CPPFLAGS		+= -fpic
+PLATFORM_CPPFLAGS		+= -fpic -mabicalls
 LDFLAGS_FINAL			+= -pie
 endif
