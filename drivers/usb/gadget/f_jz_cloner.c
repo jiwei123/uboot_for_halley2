03d20e2866339c8baa58502a4a99b78df0274b3b
YanZhengting <ztyan@ingenic.cn>
2014-06-09 12:06:15 +0800

    fix a cloner efuse write bug

--- drivers/usb/gadget/f_jz_cloner.c
+++ drivers/usb/gadget/f_jz_cloner.c
@@ -478,12 +478,12 @@ int efuse_program(struct cloner *cloner)
 		efuse_init(cloner->args->efuse_gpio);
 		enabled = 1;
 	}
-	u32 offset = cloner->cmd.write.offset;
+	u32 partation = cloner->cmd.write.partation;
 	u32 length = cloner->cmd.write.length;
 	void *addr = (void *)cloner->write_req->buf;
 	u32 r = 0;
 
-	if (r = efuse_write(addr, length, offset)) {
+	if (r = efuse_write(addr, length, partation)) {
 		printf("efuse write error\n");
 		return r;
 	}
