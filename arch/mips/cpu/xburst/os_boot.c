09e83b050ae5946e31234bfcc39a4fa2ba6e3140
YanZhengting <ztyan@ingenic.cn>
2014-08-07 16:32:04 +0800

    add paladin board for direct boot kernel for spl

---
+++ arch/mips/cpu/xburst/os_boot.c
@@ -0,0 +1,15 @@
+#include <config.h>
+#include <common.h>
+#include <div64.h>
+#include <asm/io.h>
+#include <asm/mipsregs.h>
+#include <asm/arch/ost.h>
+
+int spl_start_uboot(void)
+{               
+	return 0;
+}
+
+void spl_board_prepare_for_linux(void)
+{
+}
