2bd634094ecddffa88e8d87ab3a59beaad722777
he kunying <kyhe@ingenic.cn>
2014-09-03 09:56:34 +0800

    modify the cs num to 4

--- drivers/nand/driver/inc/nand_driver.h
+++ drivers/nand/driver/inc/nand_driver.h
@@ -8,7 +8,7 @@
 #define CS_PER_NFI		2
 #define NFI_MAX_RATE_LIMIT	(250 * 1000 * 1000)
 #elif defined(CONFIG_SOC_M200)
-#define CS_PER_NFI		2//4
+#define CS_PER_NFI		4
 #define NFI_MAX_RATE_LIMIT	(250 * 1000 * 1000)
 #endif
 
