12d1eca41df1da5c9101071084405dd17473360d
xguo <xguo_hf@ingenic.cn>
2015-03-09 16:47:40 +0800

    Modify the header file for k1mbv01
    Change-Id: Ifb162e780450ed3e2938e309f3603d0d1a8b62a7

--- include/configs/k1mbv01.h
+++ include/configs/k1mbv01.h
@@ -61,7 +61,6 @@
 #define CONFIG_DDR_CS0			1	/* 1-connected, 0-disconnected */
 #define CONFIG_DDR_CS1			0	/* 1-connected, 0-disconnected */
 #define CONFIG_DDR_DW32			1	/* 1-32bit-width, 0-16bit-width */
-#define CONFIG_MCP_H9TP32A8JDMC_PRKGM_LPDDR2
 #define CONFIG_FMT4D32UAB_25LC_LPDDR2
 
 /* #define CONFIG_DDR_DLL_OFF */
