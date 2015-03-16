d3f623cc4b5dffe790e62d4c0d1fe85d3d9ff97f
ykli <ykli@ingenic.cn>
2015-03-16 19:01:17 +0800

    Add Force cs1 to avoid ddr training error
    Change-Id: Id713073c47d5322146376f90d8852a9a9b037ec0

--- include/ddr/chips/FMT4D32UAB_25LC_LPDDR2.h
+++ include/ddr/chips/FMT4D32UAB_25LC_LPDDR2.h
@@ -9,9 +9,9 @@
  */
 /* LPDDR2 paramters */
 #define DDR_ROW 	13 /* ROW : 12 to 14 row address */
-#define DDR_ROW1 	14 /* ROW : 12 to 14 row address */
+#define DDR_ROW1 	13 /* ROW : 12 to 14 row address */
 #define DDR_COL 	9  /* COL :  8 to 10 column address */
-#define DDR_COL1 	10  /* COL :  8 to 10 column address */
+#define DDR_COL1 	9  /* COL :  8 to 10 column address */
 #define DDR_BANK8 	1  /* Banks each chip: 0-4bank, 1-8bank ,0 for falcon fpga, 1 for develop board */
 #define DDR_CL  	5  /* CAS latency: , LPDDR no */
 
@@ -33,12 +33,11 @@
 #define DDR_tRFC 	130 /* ns,  AUTO-REFRESH command period. */
 #define DDR_tMINSR 	57  /* Minimum Self-Refresh / Deep-Power-Down , tCK, no */
 #define DDR_tXP 	MAX(2, 7500)   /* EXIT-POWER-DOWN to next valid command period. ns */
-#define DDR_tMRD 	5	/* ------------unit: tCK Load-Mode-Register to next valid command period, tck, tMRW */
-#define DDR_tMRR	2	/* ------------Mode Register Read command period */
-#define DDR_tMRw	3	/* ------------Mode Register write command period */
+#define DDR_tMRD 	5	/* unit: tCK Load-Mode-Register to next valid command period, tck, tMRW */
+#define DDR_tMRR	2	/* Mode Register Read command period */
 /* new add */
-#define DDR_tCKSRE  	MAX(3, 1500) 	/* --------LPDDR2 no: Valid Clock Requirement after Self Refresh Entry or Power-Down Entry */
-#define DDR_tDLLLOCK	512 /*--------*/
+#define DDR_tCKSRE  	MAX(3, 1500) 	/* LPDDR2 no: Valid Clock Requirement after Self Refresh Entry or Power-Down Entry */
+#define DDR_tDLLLOCK	512
 #define DDR_tXSRD 	MAX(2, (DDR_tRFC + 10) * 1000)	/* LPDDR2 : Exit self-refresh to next valid command , ns, tXSR */
 #define DDR_tXS 	DDR_tXSRD	/* LPDDR2 : Exit self-refresh to next valid command , ns , tXSR */
 
@@ -47,15 +46,14 @@
 
 #define DDR_BL	 	8	/* LPDDR2 Burst length: 3 - 8 burst, 2 - 4 burst , 4 - 16 burst*/
 //#define DDR_tAL  	0	/* Additive Latency, tCK*/
-//#define DDR_tRL  	MATCH(CONFIG_SYS_MEM_FREQ,0)/* ------------4*/	/* LPDDR2: Read Latency  - 3 4 5 6 7 8 , tck*/
-//#define DDR_tWL		MATCH(CONFIG_SYS_MEM_FREQ,1)/* ----------2*/	/* LPDDR2: Write Latency - 1 2 2 3 4 4 , tck*/
-#define DDR_tRL  	3	/* ---------LPDDR2: Read Latency  - 3 4 5 6 7 8 , tck*/
-#define DDR_tWL		1	/* ---------LPDDR2: Write Latency - 1 2 2 3 4 4 , tck*/
+#define DDR_tRL  	MATCH(CONFIG_SYS_MEM_FREQ,0)/*8*/	/* LPDDR2: Read Latency  - 3 4 5 6 7 8 , tck*/
+#define DDR_tWL		MATCH(CONFIG_SYS_MEM_FREQ,1)/*4*/	/* LPDDR2: Write Latency - 1 2 2 3 4 4 , tck*/
+//#define DDR_tRL  	3	/* LPDDR2: Read Latency  - 3 4 5 6 7 8 , tck*/
+//#define DDR_tWL		1	/* LPDDR2: Write Latency - 1 2 2 3 4 4 , tck*/
 #define DDR_tCCD 	2	/* CAS# to CAS# command delay , tCK*/
-#define DDR_tRTW        (DDR_tRL + (((DDR_tDQSCKMAX * 1000) % tck_g.ps) ? \
-			(DDR_tDQSCKMAX * 1000 / tck_g.ps + 1) : \
-			(DDR_tDQSCKMAX * 1000 / tck_g.ps)) + DDR_BL / 2 + 1 - DDR_tWL) 
-#define DDR_tFAW 	MAX(8, 50 * 1000)	/* Four bank activate period, ns */
+#define DDR_tRTW        (DDR_tRL + (((DDR_tDQSCKMAX * 1000) + tck_g.ps -1)/tck_g.ps) \
+							 + DDR_BL / 2 + 1 - DDR_tWL)
+#define DDR_tFAW 	MAX(8, 60 * 1000)	/* Four bank activate period, ns */
 #define DDR_tCKE	3		/* CKE minimum pulse width, tCK */
 #define DDR_tRDLAT	(DDR_tRL - 1)
 #define DDR_tWDLAT	(DDR_tWL - 0)
@@ -63,7 +61,7 @@
 /*
  * LPDDR2 controller refcnt register
  */
-#define DDR_tREFI	3900	/* Refresh period: 4096 refresh cycles/64ms , line / 64ms */
+#define DDR_tREFI	7800	/* Refresh period: 4096 refresh cycles/64ms , line / 64ms */
 
 #define DDR_CLK_DIV 	1   /* Clock Divider. auto refresh *cnt_clk = memclk/(16*(2^DDR_CLK_DIV))*/
 
