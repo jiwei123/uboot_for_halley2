9fb65cd253b73c34438e7c69a0aeb63b4e738e71
maolei.wang <maolei.wang@ingenic.com>
2015-03-21 18:43:37 +0800

    fix BOE LCD split screen problem
    Change-Id: I666cd7ca2018351eaaca48acb4b87e0f7852d4d9

--- include/configs/watch.h
+++ include/configs/watch.h
@@ -178,7 +178,7 @@
 
 #ifdef CONFIG_VIDEO_BOE_TFT320320
 #define CONFIG_JZ_MIPI_DSI
-#define CONFIG_DEFAULT_BYTE_CLOCK	250 /* NOTE: CONFIG_DEFAULT_BYTE_CLOCK should move to lcd panel para in the future. */
+#define CONFIG_DEFAULT_BYTE_CLOCK	432 /* NOTE: CONFIG_DEFAULT_BYTE_CLOCK should move to lcd panel para in the future. */
 #endif
 
 #ifdef CONFIG_VIDEO_EDO_E1392AM1
