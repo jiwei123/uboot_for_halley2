cbf4fb6554a3f5e29d725b6e9d34d135e657d78e
HuXiaohui <xhhu_bj@ingenic.cn>
2015-08-19 17:21:47 +0800

    Merge commit 'e91ca266258d880ef315ef08e70fef1efb19deba' of ingenic-master.
    ===================
    commit e91ca266258d880ef315ef08e70fef1efb19deba
    Author: liu bo <bliu@ingenic.cn>
    Date:   Mon Aug 10 11:54:43 2015 +0800
    
        fix ota bootx BOOTARGS
    ===================

--- include/configs/phoenix.h
+++ include/configs/phoenix.h
@@ -126,9 +126,9 @@
 #define CONFIG_SPL_OS_OFFSET        (0x100000) /* spi offset of xImage being loaded */
 #define CONFIG_SPL_BOOTARGS         BOOTARGS_COMMON "ip=off init=/linuxrc rootfstype=cramfs root=/dev/mtdblock3 rw"
 #define CONFIG_SYS_SPL_ARGS_ADDR    CONFIG_SPL_BOOTARGS
-#define CONFIG_BOOTX_BOOTARGS       BOOTARGS_COMMON "ip=off init=/linuxrc rootfstype=cramfs root=/dev/mtdblock3 rw"
+#define CONFIG_BOOTX_BOOTARGS       BOOTARGS_COMMON "ip=off init=/linuxrc rootfstype=cramfs root=/dev/mtdblock4 rw"
 #undef  CONFIG_BOOTCOMMAND
-#define CONFIG_BOOTCOMMAND    "bootx sfc 0x80f00000 0x800000"
+#define CONFIG_BOOTCOMMAND    "bootx sfc 0x80f00000 0xd00000"
 #endif
 
 #define PARTITION_NUM 10
