552d9630d1be022eb3c60c2e9c8b1643b802586a
Derrick <Derrick.kznan@ingenic.com>
2016-01-05 16:25:23 +0800

    Enhance the baud rate to 115200
    Change-Id: I9c866fb739aac2974c705ec6ef2ceef1351f7bab

--- include/configs/coldwave.h
+++ include/configs/coldwave.h
@@ -57,7 +57,7 @@
 #define CONFIG_SYS_CACHELINE_SIZE	32
 
 #define CONFIG_SYS_UART_INDEX		3
-#define CONFIG_BAUDRATE			57600
+#define CONFIG_BAUDRATE			115200
 
 #define CONFIG_DDR_PARAMS_CREATOR
 #define CONFIG_DDR_HOST_CC
@@ -84,7 +84,7 @@
 /**
  * Boot arguments definitions.
  */
-#define BOOTARGS_COMMON "console=ttyS3,57600n8 mem=255M@0x0 mem=256M@0x30000000"
+#define BOOTARGS_COMMON "console=ttyS3,115200n8 mem=255M@0x0 mem=256M@0x30000000"
 
 #ifdef CONFIG_BOOT_ANDROID
     #define CONFIG_BOOTARGS BOOTARGS_COMMON " ip=off root=/dev/ram0 rw rdinit=/init"
